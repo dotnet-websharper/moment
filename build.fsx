14fe283ca3f26b7134b41ba6bab426a4e82a8fa0
Loic Denuziere <loic@denuziere.net>
2018-01-23 18:07:46 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,38 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.Moment")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFramework(fun fw -> fw.Net45)
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper.Extension("WebSharper.Moment")
-        .Embed(["moment-with-locales.min.js"; "moment-timezone-with-data.min.js"])
-        .SourcesFromProject()
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-let test =
-    bt.WebSharper.HtmlWebsite("WebSharper.Moment.Tests")
-        .SourcesFromProject()
-        .References(fun r -> 
-            [
-                r.NuGet("WebSharper.Html").Version("(,4.0)").Reference()
-                r.Project main
-            ])
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-bt.Solution [
-    main
-    test
-
-    bt.NuGet.CreatePackage()
-        .Configure(fun c ->
-            { c with
-                Title = Some "WebSharper.Moment-2.12.0"
-                LicenseUrl = Some "http://websharper.com/licensing"
-                ProjectUrl = Some "https://github.com/intellifactory/websharper.moment"
-                Description = "WebSharper Extensions for Moment.js 2.12.0"
-                RequiresLicenseAcceptance = true })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
